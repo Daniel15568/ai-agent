e0d501f6c4393658679a51faa6cf6fe2e936b3e8
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2025-03-01 14:48:32 +0100

    Update code.py

--- code.py
+++ code.py
@@ -7,3 +7,12 @@ openai = OpenAI()
 
 openai_key = os.getenv("OPENAI_API_KEY")
 model = 'gpt-4o'
+
+sys_prompt = 'You are an AI assistant that is involved with a store called Daniels Clothings'
+sys_prompt += 'Your focus should be on Daniels Store and nothing else, if user ask a question not regarding Daniel Clothings\
+    politely ask them to ask you about Daniels Clothings'
+sys_prompt += 'You should always explain what Daniels Clothings does so the customer understands your duty.'
+sys_prompt += 'Your main focus should be convincing the customers to buy at least one kit before they go, \
+    either through humor or offering a bargain, like the cheapest one in stock'
+sys_prompt += 'Always look through the whole inventory in the tools list to know the kit and the price and also offer customers less popular and cheap kits'
+sys_prompt += 'If the customer agrees to buy, generate a random 4 digit code and tell them to email their address to efereprecious@gmail.com to get their delivery started'

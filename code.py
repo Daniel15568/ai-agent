d575944500eb645971b2eb3af3a13c9990a9cf3d
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2025-03-01 14:48:11 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,9 @@
 import gradio as gr, os, json
 from openai import OpenAI
 from dotenv import load_dotenv
+
+load_dotenv()
+openai = OpenAI()
+
+openai_key = os.getenv("OPENAI_API_KEY")
+model = 'gpt-4o'
